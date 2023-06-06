0c26d52842dc8f04ec0410b1e9fbf72b1fe33910
mahru <mahrusferdous@gmail.com>
2023-06-06 10:09:59 -0400

    cost base set

--- lostcost-frontend/components/Cost.tsx
+++ lostcost-frontend/components/Cost.tsx
@@ -4,17 +4,28 @@ import tw from "tailwind-react-native-classnames";
 import React from "react";
 
 interface CostProps extends React.HTMLAttributes<HTMLDivElement> {
-    data: any;
+    data: {
+        length: number;
+        time: number;
+        distance: number;
+    };
 }
 
 const Cost: React.FC<CostProps> = ({ data }) => {
+    console.log(data);
+    const [cost, setCost] = useState<number>(0);
+
+    useEffect(() => {
+        setCost(data.distance * 10);
+    }, [data]);
+
     return (
         <View>
             <View style={tw`flex flex-row justify-between py-5 px-5`}>
                 <Text style={tw`text-base`}>
-                    {data.time} ({data.distance} km)
+                    {data.length > 0 ? data.time : 0} ({data.length > 0 ? data.distance : 0} km)
                 </Text>
-                <Text style={tw`text-3xl`}>৳{data.distance * 0.05}</Text>
+                <Text style={tw`text-3xl`}>৳{data.length > 0 ? cost : 0}</Text>
             </View>
         </View>
     );
