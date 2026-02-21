ed448c70d827b239cd01a1dcf2120f35990858f8
Mahrus Ferdous <mahrusferdous@gmail.com>
2026-02-21 00:17:55 -0500

    refactor: Update env.d.ts to include MAPTILER_KEY and improve formatting

--- lostcost-frontend/env.d.ts
+++ lostcost-frontend/env.d.ts
@@ -1,10 +1,11 @@
 declare module "@env" {
-    export const ANDROID_APP_BANNER: string;
-    export const ANDROID_APP_OPEN: string;
-    export const ANDROID_APP_ID: string;
+	export const ANDROID_APP_BANNER: string;
+	export const ANDROID_APP_OPEN: string;
+	export const ANDROID_APP_ID: string;
 
-    export const envURL: string;
-    export const lostCostURL: string;
+	export const envURL: string;
+	export const lostCostURL: string;
+	export const MAPTILER_KEY: string;
 }
 
 declare module "tailwind-react-native-classnames";
